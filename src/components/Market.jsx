0276ec47cb4e19f64fb5aa1651f647a71691bea0
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-10-08 07:41:15 +0200

    wip

--- src/components/Market.jsx
+++ src/components/Market.jsx
@@ -1,31 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState } from 'react';
 
-const fruitsApi = 'http://localhost:4000/market/fruits';
-const meatsApi = 'http://localhost:4000/market/meats';
-
-function Market() {
-  const [stock, setStock] = useState({
-    fruits: [],
-    meats: [],
+export default function Market() {
+  const [stock] = useState({
+    fruits: ['pear', 'mango', 'banana', 'kiwi'],
+    meats: ['beef', 'chicken', 'fish'],
   });
   const [cart, setCart] = useState([]);
 
   const addToCart = (item) => {
     setCart(cart.concat(item));
   };
-
-  useEffect(() => {
-    const fruitsPromise = axios.get(fruitsApi);
-    const meatsPromise = axios.get(meatsApi);
-    Promise.all([fruitsPromise, meatsPromise])
-      .then(([fruitsAxiosRes, meatsAxiosRes]) => {
-        setStock({
-          fruits: fruitsAxiosRes.data,
-          meats: meatsAxiosRes.data,
-        });
-      });
-  }, []);
 
   return (
     <div className="component">
@@ -35,9 +19,6 @@ function Market() {
     </div>
   );
 }
-
-export default Market;
-
 
 function Cart(props) {
   const { items } = props;
@@ -58,7 +39,7 @@ function Fruit(props) {
   return (
     <div>
       <span>{name}</span>
-      <button onClick={evt => addToCart(name)}>Add To Cart</button>
+      <button onClick={() => addToCart(name)}>Add To Cart</button>
     </div>
   );
 }
