c8e311634aa4f440578b3913aeda889c041d14fe
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-10-08 09:28:59 +0200

    wip

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,6 +4,7 @@ export default function Counter() {
   const [count, setCount] = useState(0);
   const increment = () => setCount(count + 1);
   const decrement = () => setCount(count - 1);
+
   return (
     <div className='component'>
       The count is {count}
