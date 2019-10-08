24fe7d22aa307d81f26071d31935cd96d8e3a6c9
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-10-08 07:48:32 +0200

    wip

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 
 export default function Counter() {
   const [count, setCount] = useState(0);
-  const increment = event => setCount(count + 1);
-  const decrement = event => setCount(count - 1);
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
   return (
     <div className='component'>
       The count is {count}
