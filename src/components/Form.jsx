d55c7ee578300dc49e7df8fa4464c1fd0e7be5c6
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-12-10 11:18:55 +0100

    wip

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,29 @@
 import React, { useState } from 'react';
 
+// step 1 design a nice initial component state
+const initialState = {
+  fname: '',
+  lname: '',
+  // isModalOpen: false,
+  // validationErrors: [],
+};
+
+// step 2
+const SUBMIT = 'SUBMIT';
+const INPUT_CHANGE = 'INPUT_CHANGE';
+const RESET = 'RESET';
+// const CLOSE_MODAL = 'CLOSE_MODAL'; // ETC
+// const ERROR = 'ERROR';
+
+// step 3
+function reducer(state, action) {
+  switch (action.type) {
+    case SUBMIT:
+      return initialState;
+    case INPUT_CHANGE: // { fname: 'a', lname: '' }, { type: INPUT_CHANGE, payload: { whichInput: 'fname', value: 'ab' } }
+  }
+}
+
 export default function Form() {
   const [formValues, setFormValues] = useState({
     fname: '',
