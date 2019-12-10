814901ac5878677c1e4d45183f04da16cbb74bfd
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-12-10 10:44:30 +0100

    wip

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,5 +1,42 @@
 import React, { useState } from 'react';
 
+// STEP 1 - DECIDE WHAT STATE THE COMPONENT NEEDS (OBJECT)
+// this thing will serve as our default initial state
+const initialState = {
+  count: 0,
+  isModalOpen: false,
+  friends: [],
+};
+
+// STEP 2 - DECIDE WHAT TYPES OF 'THINGS' MIGHT HAPPEN
+// that would result in a change of state (STRINGS)
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+const RESET = 'RESET';
+const RESET_COUNT = 'RESET_COUNT';
+const OPEN_MODAL = 'OPEN_MODAL';
+const CLOSE_MODAL = 'CLOSE_MODAL';
+
+// STEP 3 - BUILD A REDUCER FUNCTION
+// WHAT IS A REDUCER?
+//    - function that takes state and returns new (or same) state
+//    - always returns state
+//    - no mutation!!!!
+function reducer(state, action) { // { count: 7, etc } STATE { type: INCREMENT } ACTION
+  switch (action.type) {
+    case INCREMENT:
+      return { ...state, count: state.count + 1 };
+    case DECREMENT:
+      return { ...state, count: state.count - 1 };
+    case RESET:
+      return initialState;
+    case RESET_COUNT:
+      return { ...state, count: 0 };
+    default:
+      return state;
+  }
+}
+
 export default function Counter() {
   const [count, setCount] = useState(0);
   const increment = () => setCount(count => count + 1);
