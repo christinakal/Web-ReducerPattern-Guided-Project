dfaa4a629a0663e8b00c12997330c4f8bc413280
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-12-10 12:12:11 +0100

    wip

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 
 const todoList = [
   { id: '1', name: "sweeping", completed: false },
@@ -6,18 +6,44 @@ const todoList = [
   { id: '3', name: "washing dishes", completed: false },
 ];
 
+const initialState = {
+  todoList,
+  // etc
+};
+
+const MARK_TODO = 'MARK_TODO'; // we will use the same to "complete" and "uncomplete"
+// etc
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case MARK_TODO:
+      return {
+        ...state,
+        todoList: state.todoList.map(todo => {
+          if (todo.id === action.payload.id) {
+            return { ...todo, completed: action.payload.completed };
+          }
+          return todo;
+        }),
+      };
+    // other cases etc
+    default:
+      return state;
+  }
+};
+
 export default function Todos() {
-  const [todos, setTodos] = useState(todoList);
-  const markTodo = (id, isComplete) => () => {
-    setTodos(todos.map(todo => {
-      if (todo.id !== id) return todo;
-      return { id: todo.id, name: todo.name, completed: isComplete };
-    }));
-  };
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const markTodo = (id, completed) => event => dispatch({
+    type: MARK_TODO,
+    payload: { id, completed },
+  });
+
   return (
     <div className='component'>
       {
-        todos.map((todo) => (
+        state.todoList.map((todo) => (
           <div key={todo.id} style={{ color: !todo.completed ? 'red' : 'green' }}>
             {todo.name}
             <button onClick={markTodo(todo.id, true)}>Mark complete</button>
